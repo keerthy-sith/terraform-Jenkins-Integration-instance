191e4dcd7dc5c764c183ea4652eb5404e679aa0c
EC2 Default User <ec2-user@ip-172-31-83-230.ec2.internal>
2022-01-17 10:40:56 +0000

    first

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+        region="us-east-1"
+}
+resource "aws_instance" "instance-1" {
+        ami="ami-08e4e35cccc6189f4"
+        instance_type="t2.micro"
+	tags={
+		Name="git-ec2"
+	}
+}
