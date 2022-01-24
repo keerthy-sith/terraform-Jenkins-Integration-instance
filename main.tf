a7757dd733d4a458ede1deffaff20adc26e1aed3
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-24 22:05:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,5 @@ resource "aws_instance" "instance-1" {
         ami="ami-08e4e35cccc6189f4"
         instance_type="t2.micro"
 	key_name="Jenkins"
-	provisioner "file" {
-	source="index.html"
-	        destination="index.html"
-} 
-connection {
-        type="ssh"
-        host=self.public_ip   
-        user="ec2-user"
-        password="" 
-        private_key=file("Jenkins.pem")
-}
+	
 }
