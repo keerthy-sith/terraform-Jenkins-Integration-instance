20fb7e792a484cf829a857776cc4c6ee1d8843a7
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-25 19:11:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "instance-1" {
         ami="ami-08e4e35cccc6189f4"
         instance_type="t2.micro"
-	key_name="ansible"
+	key_name="Jenkins"
 	tags={
 		Name="Ansible"
 	}
